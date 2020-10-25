5f1636b648cb33bceb6ddbbddad267cac4c8c9b4
Kaleb Barrett <dev.ktbarrett@gmail.com>
2020-10-25 15:08:39 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Kaleb's S\*\*\*\*y LISP
+
+Attempt \#2 (see [old-master](https://github.com/ktbarrett/ksl/tree/old-master)).
+Making improvements to the lexer/parser for supporting significant whitespace to remove some parens.
+Added support for set and map literals, additional number literals, and better errors.
---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import pathlib
+
+
+def get_version(version_file):
+    locls = {}
+    exec(open(version_file).read(), {}, locls)
+    return locls["__version__"]
+
+
+here = pathlib.Path(__file__).parent.resolve()
+readme_file = here / "README.md"
+version_file = here / "src" / "ksl" / "version.py"
+
+
+setup(
+    name="ksl",
+    version=get_version(version_file),
+    description="Kaleb's S****y LISP",
+    long_description=readme_file.read_text(encoding="utf-8"),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ktbarrett/ksl",
+    author="Kaleb Barrett",
+    author_email="dev.ktbarrett@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Software Development :: Compilers",
+        "Programming Language :: Lisp",
+        "Programming Language :: Python :: 3",
+    ],
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    python_requires=">=3.6, <4",
+    install_requires=[],
+    entry_points={},
+)
