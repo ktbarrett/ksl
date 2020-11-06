3ce594238985955fde18295defe95ed3f25be358
Kaleb Barrett <dev.ktbarrett@gmail.com>
2020-11-05 22:32:25 -0600

    Improve engine coverage; still missing macro testing

--- src/ksl/engine.py
+++ src/ksl/engine.py
@@ -1,4 +1,6 @@
-from typing import Any, Union, Protocol, Dict, List, TypeVar, Callable, Optional
+from typing import Any, Union, Generic, Dict, List, TypeVar, Callable, Optional
+from abc import ABC, abstractmethod
+from .utils import cached
 
 
 MacroFunctionType = Callable[['ListExpression'], 'Expression']
@@ -22,56 +24,18 @@ def eval(
     return bound.eval()
 
 
-class Expression(Protocol[T]):
-    """
-    The fundamental element of computation in a LISP.
+class Expression(ABC, Generic[T]):
 
-    All elements of a LISP program are either an expression or a syntactic form (a macro).
-    Expressions are evaluated in two steps: bind() and eval().
-    The bind() step exists to resolve all names in an expression by replacing them with the
-    expression they are bound to.
-    In the bind() step, macro functions, which allow the user to define new name bindings
-    and create new syntax are also run.
-    At the end of the bind() step, all names should be resolved.
-    In the eval() step the expressions are evaluated by interpreting list expressions as
-    function calls with arguments.
-    The result of the function call is returned to the enclosing list expression until
-    it exists the top-level expression returning a result to the user.
-    eval() can
-    """
-
+    @abstractmethod
     def bind(self, variable_scope: 'Scope[Expression]', macro_scope: 'Scope[MacroFunctionType]') -> 'Expression[T]':
-        """
-        Replaces instances of Name with a bound expression and runs macro functions.
+        pass  # pragma: no cover
 
-        Macro functions can define new names and transform expressions allowing new
-        syntaxes to be defined.
-        bind() can be run on any expression, and if run more than once on the same expression
-        with the same arguments, is effectively a no-op.
-        """
-
+    @abstractmethod
     def eval(self) -> T:
-        """
-        Executed an expression that has been bound.
-
-        List Expression encode function calls and arguments to those functions.
-        The eval() step calls those functions and folds an expression tree into a single value.
-        If an unbound name is seen, an error is raised.
-        eval() can only be run safely on expressions where all names have been resolved.
-        Running eval() on the same expression will return the same argument if and only if
-        the expression is pure.
-        """
+        pass  # pragma: no cover
 
 
 class Scope(Dict[str, T]):
-    """
-    A stack of namespaces
-
-    New names are added to the current namespace.
-    Names are retrieved starting at the current namespace and travelling
-    upwards through the parents until the name is found.
-    If no name is found on retrieval, throws NameError.
-    """
 
     def __init__(self, parent: Union[None, 'Dict[str, T]'] = None):
         super().__init__()
@@ -86,16 +50,11 @@ class Scope(Dict[str, T]):
             return super().__getitem__(item)
         elif self._parent is not None:
             return self._parent[item]
-        raise NameError(f"Name '{item}' not in scope")
+        else:
+            raise KeyError(repr(item))
 
 
 class Literal(Expression[T]):
-    """
-    A expression for literal values
-
-    Since this object already has a value, bind() does nothing,
-    and eval() simply returns the value.
-    """
 
     def __init__(self, value: T, **info: Any):
         self._value = value
@@ -107,22 +66,17 @@ class Literal(Expression[T]):
 
     @property
     def info(self) -> Dict[str, Any]:
-        return self._info
+        return self._info  # pragma: no cover
 
     def bind(self, variable_scope: Scope[Expression], macro_scope: Scope[MacroFunctionType]) -> Expression:
         return self
 
+    @cached
     def eval(self) -> T:
         return self._value
 
 
 class Name(str, Expression[Any]):
-    """
-    An expression for names to be resolved later
-
-    This is a placeholder for a value that will be resolved later in the bind() step.
-    Names should not exist after the bind() step, so eval()ing a Name raises a RuntimeError.
-    """
 
     def __new__(cls, name: str, **info: Any):
         s = str.__new__(cls, name)
@@ -131,45 +85,21 @@ class Name(str, Expression[Any]):
 
     @property
     def info(self) -> Dict[str, Any]:
-        return self._info
+        return self._info  # pragma: no cover
 
     def bind(self, variable_scope: Scope[Expression], macro_scope: Scope[MacroFunctionType]) -> Expression:
-        return variable_scope[self]
+        try:
+            expr = variable_scope[self]
+        except KeyError:
+            return self
+        else:
+            return expr
 
     def eval(self) -> Any:
         raise RuntimeError("Attempted evaluating unbound name")
 
 
-class Parameter(Name):
-    """
-    A name that is captured by a function
-
-    Functions definition adds these to the scope during the bind() step
-    where they are mapped to function parameter names.
-    Function objects hold a reference to a parameter and bind an expression when the
-    function is invoked.
-    When eval()ed, the parameter acts as a proxy for the bound expression.
-    """
-
-    def set_value(self, value: Expression) -> None:
-        self._value = value
-
-    def eval(self) -> Any:
-        return self._value.eval()
-
-
 class ListExpression(tuple, Expression):
-    """
-    A ListExpression encodes a function or macro call on arguments.
-
-    During bind(), if the first element of a list expression is a macro function,
-    the macro is executed on the unbound expression list transforming it into another
-    expression list.
-    Then the resulting expression runs through bind() in case it is not fully bound.
-    During eval(), the first element of a list expression is considered a function,
-    The first value in the list is eval()ed and then the remaining unevaluated args
-    are passed to the function and the resulting value returned.
-    """
 
     def __new__(cls, sub_expressions: List[Expression], **info: Any):
         if not sub_expressions:
@@ -180,21 +110,38 @@ class ListExpression(tuple, Expression):
 
     @property
     def info(self) -> Dict[str, Any]:
-        return self._info
+        return self._info  # pragma: no cover
 
     def bind(self, variable_scope: Scope[Expression], macro_scope: Scope[MacroFunctionType]) -> Expression:
-        if len(self) >= 1:
-            func = self[0]
-            if isinstance(func, Name) and func in macro_scope:
-                macro = macro_scope[func]
-                changed = macro(self)
-                return changed.bind(variable_scope, macro_scope)
+        func = self[0]
+        if isinstance(func, Name) and func in macro_scope:
+            macro = macro_scope[func]
+            changed = macro(self)
+            return changed.bind(variable_scope, macro_scope)
         return ListExpression(
             expr.bind(variable_scope, macro_scope)
             for expr in self)
 
+    @cached
     def eval(self) -> Any:
         func, *args = self
         func = func.eval()
         res = func(args)
         return res
+
+
+class Function:
+
+    def __init__(self, params: List[str], body: Expression, name: Optional[str] = None):
+        self._params = params
+        self._body = body
+        self._name = name if name is not None else "(anonymous)"
+
+    def __call__(self, args: List[Expression]):
+        if len(args) != len(self._params):
+            raise TypeError(f"")
+        params = Scope()
+        for param, arg in zip(self._params, args):
+            params[param] = arg
+        body = self._body.bind(params, {})
+        return body.eval()
--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,5 +1,5 @@
 import pytest
-from ksl.engine import Scope, Literal, ListExpression, Name, eval as ksleval
+from ksl.engine import Scope, Literal, ListExpression, Name, Function, eval as ksleval
 
 
 def test_literal():
@@ -13,11 +13,8 @@ def test_name_fails():
 
     test_expr = Name('jeb')
 
-    with pytest.raises(NameError):
-        ksleval(test_expr)
-
     with pytest.raises(RuntimeError):
-        test_expr.eval()
+        ksleval(test_expr)
 
 
 def test_name_found():
@@ -36,11 +33,11 @@ def test_list_expression():
     env = Scope()
     env['+'] = Literal(lambda args: sum(arg.eval() for arg in args))
 
-    test_expr = ListExpression([
+    test_expr = ListExpression((
         Name('+'),
         Literal(1),
         Literal(2),
-        Literal(3)])
+        Literal(3)))
 
     r = ksleval(test_expr, env)
     assert r == 6
@@ -49,4 +46,70 @@ def test_list_expression():
 def test_empty_list_expression():
 
     with pytest.raises(ValueError):
-        ListExpression([])
+        ListExpression(())
+
+
+def test_function():
+
+    params = ['a', 'b']
+
+    body = ListExpression((
+        Literal(lambda args: sum(arg.eval() for arg in args)),
+        Name('a'),
+        Name('b')))
+
+    func = Literal(Function(params, body, name="add"))
+
+    test_expr = ListExpression((
+        Literal(lambda args: sum(arg.eval() for arg in args)),
+        ListExpression((
+            func,
+            Literal(1),
+            Literal(2))),
+        ListExpression((
+            func,
+            Literal(3),
+            Literal(4)))))
+
+    r = ksleval(test_expr)
+    assert r == 10
+
+
+def test_function_not_enough_args():
+
+    params = ['a', 'b']
+
+    body = ListExpression((
+        Literal(lambda args: sum(arg.eval() for arg in args)),
+        Name('a'),
+        Name('b')))
+
+    func = Literal(Function(params, body, name="add"))
+
+    test_expr = ListExpression((
+        func,
+        Literal(5)))
+
+    with pytest.raises(TypeError):
+        ksleval(test_expr)
+
+
+def test_function_too_many_args():
+
+    params = ['a', 'b']
+
+    body = ListExpression((
+        Literal(lambda args: sum(arg.eval() for arg in args)),
+        Name('a'),
+        Name('b')))
+
+    func = Literal(Function(params, body, name="add"))
+
+    test_expr = ListExpression((
+        func,
+        Literal(5),
+        Literal(6),
+        Literal('wew')))
+
+    with pytest.raises(TypeError):
+        ksleval(test_expr)
