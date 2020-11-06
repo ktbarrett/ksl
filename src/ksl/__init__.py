40a41e85e9edc5b38502c4d8a90bb29fbceb47a3
Kaleb Barrett <dev.ktbarrett@gmail.com>
2020-11-06 00:51:37 -0600

    Implement top-level run func

--- src/ksl/__init__.py
+++ src/ksl/__init__.py
@@ -1,5 +1,19 @@
+from typing import Union, Optional, Any
+from .engine import eval, Expression, Scope, MacroFunctionType
+from .parser import parse
+
 __version__ = '0.1.dev0'
 
 
-from .engine import eval
-from .parser import parse
+def run(
+    expr: Union[Expression, str, None] = None,
+    *,
+    filename: Optional[str] = None,
+    variable_scope: Optional['Scope[Expression]'] = None,
+    macro_scope: Optional['Scope[MacroFunctionType]'] = None
+) -> Any:
+    if isinstance(expr, Expression):
+        return eval(expr, variable_scope=variable_scope, macro_scope=macro_scope)
+    else:
+        parsed_expr = parse(expr, filename=filename)
+        return eval(parsed_expr, variable_scope=variable_scope, macro_scope=macro_scope)
