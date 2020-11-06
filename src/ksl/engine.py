3a7db35379918e52cd96cfb63d25de2f6cf942f5
Kaleb Barrett <dev.ktbarrett@gmail.com>
2020-11-05 23:06:29 -0600

    make mypy happy in engine

--- src/ksl/engine.py
+++ src/ksl/engine.py
@@ -1,9 +1,15 @@
-from typing import Any, Union, Generic, Dict, List, TypeVar, Callable, Optional
+from typing import Any, Union, Dict, List, TypeVar, Callable, Optional, Iterable
 from abc import ABC, abstractmethod
 from .utils import cached
 
 
-MacroFunctionType = Callable[['ListExpression'], 'Expression']
+ValueType = Any
+
+FunctionType = Callable[[List['Expression']], 'ValueType']
+
+# mypy does not support this in any way
+# MacroFunctionType = Callable[['ListExpression', 'Scope[ValueType]', 'Scope[MacroFunctionType]'], 'Expression']
+MacroFunctionType = Callable[['ListExpression', 'Scope[ValueType]', 'Scope'], 'Expression']
 
 
 T = TypeVar('T')
@@ -14,35 +20,34 @@ def eval(
     variable_scope: Optional['Scope[Expression]'] = None,
     macro_scope: Optional['Scope[MacroFunctionType]'] = None
 ) -> Any:
-    """
-    """
+    from .builtins import builtin_variables, builtin_macros  # noqa: E402
     if variable_scope is None:
-        from .builtins import builtin_variables as variable_scope
+        variable_scope = builtin_variables
     if macro_scope is None:
-        from .builtins import builtin_macros as macro_scope
+        macro_scope = builtin_macros
     bound = expr.bind(variable_scope, macro_scope)
     return bound.eval()
 
 
-class Expression(ABC, Generic[T]):
+class Expression(ABC):
 
     @abstractmethod
-    def bind(self, variable_scope: 'Scope[Expression]', macro_scope: 'Scope[MacroFunctionType]') -> 'Expression[T]':
+    def bind(self, variable_scope: 'Scope[Expression]', macro_scope: 'Scope[MacroFunctionType]') -> 'Expression':
         pass  # pragma: no cover
 
     @abstractmethod
-    def eval(self) -> T:
+    def eval(self) -> ValueType:
         pass  # pragma: no cover
 
 
 class Scope(Dict[str, T]):
 
-    def __init__(self, parent: Union[None, 'Dict[str, T]'] = None):
+    def __init__(self, parent: Union[None, Dict[str, T]] = None):
         super().__init__()
         self._parent = parent
 
     @property
-    def parent(self) -> Union[None, 'Dict[str, T]']:
+    def parent(self) -> Union[None, Dict[str, T]]:
         return self._parent
 
     def __getitem__(self, item: str) -> T:
@@ -54,7 +59,7 @@ class Scope(Dict[str, T]):
             raise KeyError(repr(item))
 
 
-class Literal(Expression[T]):
+class Literal(Expression):
 
     def __init__(self, value: T, **info: Any):
         self._value = value
@@ -72,11 +77,12 @@ class Literal(Expression[T]):
         return self
 
     @cached
-    def eval(self) -> T:
+    def eval(self) -> ValueType:
         return self._value
 
 
-class Name(str, Expression[Any]):
+class Name(str, Expression):
+    _info: Dict[str, Any]
 
     def __new__(cls, name: str, **info: Any):
         s = str.__new__(cls, name)
@@ -95,13 +101,14 @@ class Name(str, Expression[Any]):
         else:
             return expr
 
-    def eval(self) -> Any:
+    def eval(self) -> ValueType:
         raise RuntimeError("Attempted evaluating unbound name")
 
 
 class ListExpression(tuple, Expression):
+    _info: Dict[str, Any]
 
-    def __new__(cls, sub_expressions: List[Expression], **info: Any):
+    def __new__(cls, sub_expressions: Iterable[Expression], **info: Any):
         if not sub_expressions:
             raise ValueError("List expressions cannot be empty")
         s = tuple.__new__(cls, sub_expressions)
@@ -116,14 +123,14 @@ class ListExpression(tuple, Expression):
         func = self[0]
         if isinstance(func, Name) and func in macro_scope:
             macro = macro_scope[func]
-            changed = macro(self)
+            changed = macro(self, variable_scope, macro_scope)
             return changed.bind(variable_scope, macro_scope)
         return ListExpression(
             expr.bind(variable_scope, macro_scope)
             for expr in self)
 
     @cached
-    def eval(self) -> Any:
+    def eval(self) -> ValueType:
         func, *args = self
         func = func.eval()
         res = func(args)
@@ -140,8 +147,8 @@ class Function:
     def __call__(self, args: List[Expression]):
         if len(args) != len(self._params):
             raise TypeError(f"")
-        params = Scope()
+        params: Scope[Expression] = Scope()
         for param, arg in zip(self._params, args):
             params[param] = arg
-        body = self._body.bind(params, {})
+        body = self._body.bind(params, Scope[MacroFunctionType]())
         return body.eval()
